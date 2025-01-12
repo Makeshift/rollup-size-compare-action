94b9e3ebe6737874400e82ce835218bf089d4a7b
Connor Bell (Makeshift) <git@connor-bell.com>
2025-01-12 12:01:48 +0000

    Remove some unneeded comments

--- src/to-comment-body.ts
+++ src/to-comment-body.ts
@@ -3,7 +3,7 @@ import {
   printChunkModulesTable,
   printTotalAssetTable
 } from './print-markdown'
-import type {WebpackStatsDiff} from './types'
+import type { WebpackStatsDiff } from './types'
 
 export function getIdentifierComment(key: string): string {
   return `<!--- bundlestats-action-comment${key ? ` key:${key}` : ''} --->`
@@ -15,11 +15,7 @@ export function getCommentBody(
   title: string
 ): string {
   return `
-### Bundle Stats${title ? ` — ${title}` : ''}
-
-Hey there, this message comes from a [GitHub action](https://github.com/twk3/rollup-size-compare-action) that helps you and reviewers to understand how these changes affect the size of this project's bundle.
-
-As this PR is updated, I'll keep you updated on how the bundle size is impacted.
+## Bundle Stats${title ? ` — ${title}` : ''}
 
 ${printTotalAssetTable(statsDiff)}
 ${chunkModuleDiff ? `${printChunkModulesTable(chunkModuleDiff)}\n` : ''}
